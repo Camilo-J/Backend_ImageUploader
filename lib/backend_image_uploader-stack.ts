733bf10af548e9159595a559b0d127f9ee94c378
Camilo-J <josecamilo1902@outlook.com>
2024-03-25 17:43:35 -0500

    refactor: update code for backend image uploader stack

--- lib/backend_image_uploader-stack.ts
+++ lib/backend_image_uploader-stack.ts
@@ -1,16 +1,22 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { config } from "dotenv";
+import { BaseStack } from "./stacks/base-stack";
+config();
 
 export class BackendImageUploaderStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const baseStack = new BaseStack(this, "BaseStack");
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BackendImageUploaderQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new cdk.CfnOutput(this, "apiEndpoint", {
+      value: baseStack.apiEndpoint.url,
+    });
+
+    new cdk.CfnOutput(this, "bucket", {
+      value: baseStack.bucket.bucketName,
+    });
   }
 }
